8e255ec1a1a03bd2ff899cb1aa4e4355e260637c
Ephraim Djeket <djeketefra10@hotmail.com>
2024-08-02 19:25:25 +0200

    Updated some tailwind code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customBlack: "#0e0e0e",
+        /** Colors */
+        customGrey900: "hsl(0, 0%, 8%)",
+        customGrey800: "hsl(0, 0%, 12%)",
+        customGrey700: "hsl(0, 0%, 20%)",
+        customGreen: "hsl(75, 94%, 57%)",
+        customDarkBg: "#0E0E0E",
         customGray: "#1F1F1F",
         customDark: "#141414",
+        /** Font Family */
+        fontFamily: {
+          "Inter": ["Inter", "sans-serif"],
+        },
       },
     },
   },
